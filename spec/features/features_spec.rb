e227d73ddd0feb3fd9b7bd20e8abb2bca6536758
Joey Cho <cjy12182000@naver.com>
2019-02-27 07:28:26 +0000

    Automatically backed up by Learn

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe "homepage", :type => :view do
     expect(page).to have_link("Login", href:'/login')
   end
 
- 
+
   it "When the user is logged in, the page should say, `hi, #{username}`, and provide a logout link" do
     visit '/'
     session[:name] = "Jakob"
-    expect(page).to have_text(`hi, #{session[:name]}`) 
+    expect(page).to have_text(`hi, #{session[:name]}`)
     expect(page).to have_link("Logout", href:'/logout')
   end
 
