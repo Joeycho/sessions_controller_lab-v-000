923223a04fb6ec61cd380ce28c7e92281490efc6
Joey Cho <cjy12182000@naver.com>
2019-02-27 08:00:02 +0000

    Automatically backed up by Learn

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -4,13 +4,13 @@ require 'spec_helper'
 describe "homepage" do
   it "When the user is not logged in, the page should show them a login link" do
     visit '/'
-    session[:name] = nil
+    let(session[:name]){nil}
     expect(page).to have_link("Login", href:'/login')
   end
 
   it "When the user is logged in, the page should say, `hi, #{name}`, and provide a logout link" do
     visit '/'
-    session[:name] = "Jakob"
+    let(session[:name]){"Jakob"}
     expect(page).to have_text(`hi, #{session[:name]}`)
     expect(page).to have_link("Logout", href:'/logout')
   end
