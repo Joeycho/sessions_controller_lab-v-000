0809a7b8994533259c7a29f07665c38a15a19122
Joey Cho <cjy12182000@naver.com>
2019-02-27 08:00:36 +0000

    Automatically backed up by Learn

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 describe "homepage" do
   it "When the user is not logged in, the page should show them a login link" do
     visit '/'
-    let(session[:name]){nil}
+    let(:session[:name]){nil}
     expect(page).to have_link("Login", href:'/login')
   end
 
