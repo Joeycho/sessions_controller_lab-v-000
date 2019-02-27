480a09190db1e27656504d6e8b42546f50d3b430
Joey Cho <cjy12182000@naver.com>
2019-02-27 07:23:25 +0000

    Automatically backed up by Learn

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe "homepage", :type => :view do
   it "When the user is not logged in, the page should show them a login link" do
     visit '/'
     session[:user_id] = nil
-    expect(page).to have_link("Login", href:'/login') 
+    expect(page).to have_link("Login", href:'/login')
   end
 end
